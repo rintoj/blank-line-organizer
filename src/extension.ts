1a36501da908564a2c955cd80a3fbba7c80ca8f6
Rinto Jose <rinto.jose@tcs.com>
2017-01-10 22:02:09 +0530

    setting added

--- src/extension.ts
+++ src/extension.ts
@@ -1,48 +1,104 @@
 'use strict';
 
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
+// import vscode
 import * as vscode from 'vscode';
 
-const multipleEmptyLines = true;
+// constance
+const NAME = 'emptyLine';
+const COMMAND = 'remove';
+const CONTEXT_SAVE = 'save';
+const CONTEXT_COMMAND = 'command';
 
-function removeEmptyLines(content) {
-    return (content || '').replace(/\n\n+/g, multipleEmptyLines ? '\n\n' : '\n');
+// configuration definition
+interface ExtensionConfig {
+    keepOneEmptyLine?: boolean;
+    triggerOnSave?: boolean;
+    insertLineAfterBlock?: boolean;
+    languageIds?: string[];
 }
 
-function getSelection(selection, document) {
-    if (selection.start.line !== selection.end.line ||
-        selection.start.character !== selection.end.character) return selection;
+// default configuration
+let config: ExtensionConfig = {
+    keepOneEmptyLine: true,
+    triggerOnSave: true,
+    insertLineAfterBlock: true,
+    languageIds: ['javascript', 'typescript', 'json']
+};
 
-    return new vscode.Range(1, 1, document.lineCount, 1);
+// read extension configuration
+function setDefaultConfig() {
+    // vscode.workspace.getConfiguration('editor').
 }
 
-function doAction() {
+// read extension configuration
+function readConfig() {
+    console.log('reading configuration....', config);
+    let settings = vscode.workspace.getConfiguration(NAME);
+    config = Object.assign({}, config, settings);
 
-    var editor = vscode.window.activeTextEditor;
-    if (!editor) {
-        return; // No open text editor
+    console.log('SETTINgs', settings, config);
+}
+
+// check for language validity
+function isValidLanguage(languageId) {
+    if (config == undefined || !(config.languageIds instanceof Array) || config.languageIds.length === 0) return true;
+    return config.languageIds.find(id => id.toLowerCase() === languageId.toLowerCase()) != undefined;
+}
+
+// remove empty lines
+function processLines(content) {
+    if (config.insertLineAfterBlock) {
+        content = (content || '').replace(/\}[\s\t]*\n+/g, '}\n\n');
+        content = (content || '').replace(/\}[\s\t]*;[\s\t]*\n+/g, '};\n\n');
     }
+    return (content || '').replace(/\n[\s\t]*\n+/g, (config.keepOneEmptyLine !== true) ? '\n' : '\n\n');
+}
 
+function doAction(event) {
+
+    // get active text editor
+    var editor = vscode.window.activeTextEditor;
+
+    // do nothing if 'doAction' was triggered by save and 'removeOnSave' is set to false
+    if (event === CONTEXT_SAVE && config.triggerOnSave !== true) return;
+
+    // do nothing if no open text editor
+    if (!editor) return;
+
+    // do nothing if not valid language
+    if (event !== CONTEXT_COMMAND && !isValidLanguage(editor.document.languageId)) return;
+
+    // identify selection
     let selection: any = editor.selection;
     if (selection.start.line === selection.end.line &&
         selection.start.character === selection.end.character) {
         selection = new vscode.Range(1, 1, editor.document.lineCount, 1);
     }
+
+    // select text
     var text = editor.document.getText(selection);
 
+    // format text
     editor.edit((edit) => {
-        edit.replace(selection, removeEmptyLines(text));
+        edit.replace(selection, processLines(text));
     });
 }
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
+// when extension is activated
 export function activate(context: vscode.ExtensionContext) {
-    context.subscriptions.push(vscode.commands.registerCommand('extension.remove-empty-lines', doAction));
-    context.subscriptions.push(vscode.workspace.onDidSaveTextDocument(doAction));
+
+    // initialize configuration
+    readConfig();
+
+    // reload configuration on change
+    vscode.workspace.onDidChangeConfiguration(readConfig);
+
+    // register 'emptyLine.remove' command
+    context.subscriptions.push(vscode.commands.registerCommand(`${NAME}.${COMMAND}`, () => doAction(CONTEXT_COMMAND)));
+
+    // listen for save event
+    context.subscriptions.push(vscode.workspace.onDidSaveTextDocument(() => doAction(CONTEXT_SAVE)));
 }
 
-// this method is called when your extension is deactivated
-export function deactivate() {
-}
+// when extension is deactivated
+export function deactivate() { }
